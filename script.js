38aab8690b5efd9e1d594c344dc6488c36c353a4
LilyPutriAndini <2300016107@webmail.uad.ac.id>
2024-10-22 17:33:35 +0700

    Attempt 1 - 08.45

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-function generatePassword(length, options) {
-    let characters = '';
-    if (options.includeUppercase) characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (options.includeLowercase) characters += 'abcdefghijklmnopqrstuvwxyz';
-    if (options.includeNumbers) characters += '0123456789';
-    if (options.includeSpecialChars) characters += '!@#$%^&*()_+[]{}|;:,.<>?';
+export const generatePassword = (length, options) => {
+    const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const numbers = "0123456789";
+    const specialChars = "!@#$%^&*()";
+
+    let characterSet = '';
+    if (options.includeUppercase) characterSet += uppercase;
+    if (options.includeLowercase) characterSet += lowercase;
+    if (options.includeNumbers) characterSet += numbers;
+    if (options.includeSpecialChars) characterSet += specialChars;
+
+    if (characterSet === '') {
+        throw new Error('At least one character type must be selected.');
+    }
 
     let password = '';
     for (let i = 0; i < length; i++) {
-        password += characters.charAt(Math.floor(Math.random() * characters.length));
+        const randomIndex = Math.floor(Math.random() * characterSet.length);
+        password += characterSet[randomIndex];
     }
 
     return password;
-}
-
-module.exports = { generatePassword };
+};
--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 // Dont change anything in this file
 
-const { expect } = require('chai');
-const { generatePassword } = require('../script.js');
+import { expect } from 'chai';
+import { generatePassword } from '../script.js';
 
 let totalTests = 0;
 let passedTests = 0;
@@ -80,20 +80,5 @@ describe('Password Generator Tests', () => {
         console.log(`\nTotal tests: ${totalTests}`);
         console.log(`Passed: ${passedTests}`);
         console.log(`Failed: ${failedTests}`);
-    });
-});import { assert } from 'chai';
-import { generatePassword } from '../script.js';
-
-describe('generatePassword', function() {
-    it('should generate a password of the correct length', function() {
-        const options = { includeUppercase: true, includeLowercase: true, includeNumbers: true, includeSpecialChars: true };
-        const password = generatePassword(12, options);
-        assert.lengthOf(password, 12);
-    });
-   
-    it('should include uppercase letters if specified', function() {
-        const options = { includeUppercase: true, includeLowercase: false, includeNumbers: false, includeSpecialChars: false };
-        const password = generatePassword(12, options);
-        assert.match(password, /[A-Z]/);
     });
 });
